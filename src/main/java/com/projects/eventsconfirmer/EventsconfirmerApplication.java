211e451ae89c08e28144c5a08a24cf5ce13b8646
Luiz Fernando <luizfernandosant26@gmail.com>
2025-06-16 15:43:56 -0300

    Project created

---
+++ src/main/java/com/projects/eventsconfirmer/EventsconfirmerApplication.java
@@ -0,0 +1,11 @@
+package com.projects.eventsconfirmer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsconfirmerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EventsconfirmerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/projects/eventsconfirmer/EventsconfirmerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.eventsconfirmer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsconfirmerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
